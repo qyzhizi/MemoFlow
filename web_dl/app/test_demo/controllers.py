9e5cddc4a8fbe2838ccf86e54c9e52fb131d93fc
qyzhizi <2830942138@qq.com>
2022-08-01 17:16:24 +0800

    [add] 添加了dependency provider 没有添加stevedore驱动机制

--- web_dl/app/test_demo/controllers.py
+++ web_dl/app/test_demo/controllers.py
@@ -3,7 +3,12 @@
 from webob.response import Response
 
 from web_dl.common import wsgi
+from web_dl.common import dependency
+
+
+@dependency.requires('test_api')
 class Test(wsgi.Application):
     def test(self, req, name):
-        return Response("hello world %s" % name)
+        test = self.test_api.test
+        return Response("hello world %s, %s" % (name, test))
 
