dba0a7af40f69112897da0d3f760ffda7da5d019
qyzhizi <2830942138@qq.com>
2023-03-28 05:12:18 +0800

    update diary_log

--- web_dl/app/diary_log/controllers.py
+++ web_dl/app/diary_log/controllers.py
@@ -32,7 +32,7 @@ class DiaryLog(wsgi.Application):
         processed_content = self.diary_log_api.process_content(diary_log['content'])
 
         # 保存到本地数据库
-        self.diary_log_api.save_log(processed_content)
+        self.diary_log_api.save_log(diary_log['content'])
 
         # 发送到浮墨笔记
         flomo_post_data = {"content": processed_content}
