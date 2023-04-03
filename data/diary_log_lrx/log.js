c3fbaa11d4965549b967c34c9bbca716a567ca5f
qyzhizi <2830942138@qq.com>
2023-04-04 06:55:03 +0800

    update log.js 实现表单中 tab  的缩进与反缩进

--- data/diary_log_lrx/log.js
+++ data/diary_log_lrx/log.js
@@ -62,4 +62,52 @@ $(function() {
             }
         });
     });
+
+    var txtInput = document.getElementById('log');
+    txtInput.addEventListener('keydown', function(e) {
+        if (e.keyCode === 9) { // 按下Tab键或Shift+Tab键
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+            var value = this.value;
+            var selectedText = value.substring(start, end);
+            if (e.shiftKey) { // 按下Shift键 需要反缩进
+                var unindentedText = selectedText.split('\n').map(function(line) {
+                    var leadingTabs = line.match(/^\t+/); // 匹配行首的制表符
+                    if (leadingTabs) { // 存在制表符
+                        // 将制表符替换为四个空格，然后去掉前四个空格或制表符
+                        let i = 0;
+                        while (line[i] === '\t') {i++;}
+                        const leadingSpaces = '    '.repeat(i); // 生成 i 个空格的字符串
+                        line = leadingSpaces + line.substring(i); // 将生成的空格字符串和剩余的字符串拼接返回
+                        return line.substring(4);
+                    } else if (line.match(/^ {1}/)) { // 前面至少有一个空格时
+                        const leadingSpaces = line.match(/^ {0,4}/)[0];
+                    return line.substring(leadingSpaces.length)
+                    } else {
+                        return line;
+                    }
+                }).join('\n');
+                this.value = value.substring(0, start) + unindentedText + value.substring(end);
+                this.selectionStart = start;
+                this.selectionEnd = end - (selectedText.length - unindentedText.length);
+            } else { // 没有按下Shift键
+                var indentedText = selectedText.split('\n').map(function(line) {
+                    let i = 0;
+                    while (line[i] === '\t') {i++;}
+                    j = i+1 // 使用4个空格进行正向缩进
+                    const leadingSpaces = '    '.repeat(j); // 生成 i 个空格的字符串
+                    return leadingSpaces + line.substring(i); // 将生成的空格字符串和剩余的字符串拼接返回
+                }).join('\n');
+                this.value = value.substring(0, start) + indentedText + value.substring(end);
+                if (selectedText.length){
+                    this.selectionStart = start;
+                    this.selectionEnd = end + (indentedText.length - selectedText.length);
+                } else {
+                    this.selectionEnd = end + (indentedText.length - selectedText.length);
+                    this.selectionStart = this.selectionEnd;
+                }
+            }
+            e.preventDefault();
+        }
+    });     
 });
