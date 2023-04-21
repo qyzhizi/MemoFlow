22a89f5070bd5868fad4da00197a0662da2ba97d
qyzhizi <2830942138@qq.com>
2023-04-21 19:35:07 +0800

    update conf

--- web_dl/db/__init__.py
+++ web_dl/db/__init__.py
@@ -4,7 +4,7 @@ from web_dl.conf import CONF
 
 DATA_BASE_PATH = CONF.diary_log['data_base_path']
 DIARY_LOG_TABLE = CONF.diary_log['diary_log_table']
-REVIEW_DIARY_LOG = CONF.diary_log['review_diary_log']
+REVIEW_DIARY_LOG = CONF.diary_log['review_diary_log_table']
 
 def init_db():
     diary_log.init_db_diary_log(data_base_path=DATA_BASE_PATH, table_name=DIARY_LOG_TABLE)
