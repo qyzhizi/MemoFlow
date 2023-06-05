1b9d7a2a8299743e65e674b5827f5183a4a602da
qyzhizi <2830942138@qq.com>
2023-06-06 03:36:26 +0800

    rename web-dl to memocard

--- memocard/conf/server.py
+++ memocard/conf/server.py
@@ -3,7 +3,7 @@ from oslo_config import cfg
 # 声明配置项
 SEVER_OPTS = [     
     cfg.StrOpt('server_conf_path',
-               default='etc/memocard/web-dl.conf',
+               default='etc/memocard/memocard.conf',
                help='服务配置文件的路径'),
 ]
 
