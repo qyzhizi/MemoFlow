251c114288bc0b752d096a722f8caaa62ab8ebf2
qyzhizi <2830942138@qq.com>
2024-05-13 04:31:41 +0800

    update url regex

--- memoflow/data/client/diary_log/log.js
+++ memoflow/data/client/diary_log/log.js
@@ -1519,7 +1519,8 @@ function processLogEntryText2(log_entry){
     const inlinePattern = {regex:/(?<!``)(`[^`]+`)/g, type: 'inLinecodeBlock'}
     const otherPatterns = [
         {regex:/((?<=\x20|^)(?<![#＃])[#＃]{1}(?![#＃])[/\w\u4e00-\u9fff]+(?=[\x20\n]|$))/g, type: 'tag'},
-        {regex:/(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&/=]*))/g, type: 'url'},
+        // {regex:/(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&/=]*))/g, type: 'url'},
+        {regex:/(https?:\/\/(?:[a-zA-Z0-9.-]+|\d{1,3}(?:\.\d{1,3}){3})(?::\d+)?(?:\/[-a-zA-Z0-9@:%_\+.~#?&/=]*)?)/g, type: 'url'},
         // {regex:/(?:\s|\r?\n)*?\$\$([\s\S]*?)\$\$(?:\s|\r?\n)*?/g, type: 'MulLineslatex'},
         {regex:/(?:[\x20]*\r?\n?|\r?\n[\x20]*)\$\$([\s\S]*?)\$\$(?:$|[\x20]*\r?\n?)/g, type: 'MulLineslatex'},
         {regex:/(?:\$|\\\[|\\\()([\s\S]*?)(?:\$|\\\]|\\\))/g, type: 'InLineslatex'},
